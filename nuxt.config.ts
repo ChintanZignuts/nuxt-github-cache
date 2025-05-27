26e3f9dd9a5364f22b06cca64a6f51ce91034f53
ChitanZignuts <chintanr@zignuts.com>
2025-05-27 11:33:12 +0530

    add Redis storage configuration to Nuxt 3 application

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  nitro: {
+    storage: {
+      redis: {
+        driver: "redis",
+        port: 6379,
+        host: "127.0.0.1",
+      },
+    },
+  },
+  devtools: { enabled: true },
+});
